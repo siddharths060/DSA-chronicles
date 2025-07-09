448152a247a59bd4423635d53a5a36d56aa49a3b
siddharths060 <siddharthsrivishwa@gmail.com>
2025-07-09 22:12:12 +0530

    merge-sort

---
+++ Algoritms/merge-sort/merge-sort.cpp
@@ -0,0 +1,6 @@
+/*
+ * merge sort is much more efficient than selection,bubble and insertion sort
+ * it uses recursion
+ * main concept of merge-sort is DIVIDE AND MERGE
+ * divide the input till you reach single eleements, then while merging we sort them and merge
+ *
--- Algoritms/selection-sort/selection_sort.cpp
+++ Algoritms/selection-sort/selection_sort.cpp
@@ -4,7 +4,41 @@ Select the minimum element and swap it
 basically, select a minimum element and swap it with the current index or pointer element
 it requires 2 for loops, i that starts from 0 to n-1(not included) and j starts from 1 to n-1(not included)
 
+pseudo code:
+outer for loop i goes from 0 to n-2
+initially assign mini to i
+inner for loop j goes from i to n-1
+check if arr[j] is less than arr[mini]
+assign mini to j 
+if so swap the elements else leave it(ie, they are sorted)
 
+
+dry run : 
+consider this example : 
+13 46 24 52 20 9
+
+i = 0, mini = i = 0, outerloop i = 0
+
+j = i = 0, if 13 < 13 false
+j = i = 1, is 46 < 13 false
+j = i = 2, is 24 < 13 false
+j = i = 3, is 52 < 13 false
+j = i = 4, is 20 < 13 false
+j = i = 5, is 9 < 13 true
+mini = j = 5
+swap(9,13)
+
+9 46 24 52 20 13
+
+i = 1, mini = i = 1, outerloop i = 1
+
+j = i = 0, if 9 < 9 false
+j = 1, if 46 < 46 false
+j = 2, if 24 < 46 true
+mini = j = 2
+swap(46,24)
+
+9 24 24 52 20 13
 
 */
 
