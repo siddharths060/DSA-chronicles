00db0a08b31fa8bb48791d16dd8e8bd8686151f3
siddharths060 <siddharthsrivishwa@gmail.com>
2025-07-10 23:00:59 +0530

    remove-nth-node code

--- NeetCode150/LinkedList/remove-nth-node/remove-nth-node-LL.cpp
+++ NeetCode150/LinkedList/remove-nth-node/remove-nth-node-LL.cpp
@@ -1,90 +1,38 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int size = 0;
+        ListNode* sizePointer = head;
+        while(sizePointer != nullptr){
+            size++;
+            sizePointer = sizePointer->next;
+        }
 
+        // if n is larger than size, just return head (invalid input)
+        if(n > size){
+            return head;
+        } 
 
-#include<iostream>
-#include<vector>
+        // if we're deleting the head
+        if(n == size){
+            ListNode* temp = head;
+            head = head->next;
+            delete temp;
+            return head;
+        }
 
-using namespace std;
+        int count = 0;
+        ListNode* curr = head;
+        while(count < size - n - 1){
+            curr = curr->next;
+            count++;
+        }
 
-class Node
-{
-public: 
-int data;
-Node* next;
+        ListNode* temp = curr->next;
+        curr->next = temp->next;
+        delete temp;
 
-Node(int data){
-this->data = data;
-next = nullptr;
-}
-
-Node(int data, Node* next){
-this->data = data;
-next = nullptr;
-}
-
+        return head;
+    }
 };
 
-void traverse(Node* head)
-{
-if(head == nullptr){
-cout<<"no elements"<<endl;
-return;
-}
-Node* curr = head;
-while(curr != nullptr){
-cout<<curr->data<<" ";
-curr = curr->next;
-}
-cout<<endl;
-
-
-Node* convertToLL(vector<int> &arr){
-
-	if(arr.size() == 0 ){
-		cout<<"no elements in array"<<endl;
-		return nullptr;
-	}
-	Node* head = nullptr;
-	Node* tail = nullptr;
-
-	for(int i = 0; i < arr.size(); i++){
-		Node* newNode = new Node(arr[i]);
-		if(head == nullptr){
-			head = newNode;
-			tail = newNode;
-
-		}
-		else{
-			tail->next = newNode;
-			tail = tail->next; // incrementing tail we can also do it this way, tail = newNode
-		}
-
-		
-	}
-
-	return head;	
-}
-
-
-void removeNthNode(Node* head){
-Node* curr = head;
-int count = 0;
-
-while(curr!=nullptr){
-count++;
-curr = curr->next;
-}
-
-cout<<count<<endl;
-
-cout<<curr->data<<endl;
-
-}
-
-
-int main(){
-vector<int> arr = {0,1,2,3,4,5};
-Node* head = convertToLL(arr);
-traverse(head);
-
-return 0;
-}
