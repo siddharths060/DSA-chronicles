e6f72ef19571803808501c4fe752f6c2fa9ffc7f
siddharths060 <siddharthsrivishwa@gmail.com>
2025-07-18 23:37:18 +0530

    brute force method for longest palindromic substring

--- NeetCode150/twopointer/longest_palandromic_substring.cpp
+++ NeetCode150/twopointer/longest_palandromic_substring.cpp
@@ -30,3 +30,47 @@ public:
         return res;
     }
 };
+/*
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(const string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    string longestPalindrome(string s) {
+        int n = s.size();
+        int maxLen = 0;
+        int start = 0;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                if (isPalindrome(s, i, j)) {
+                    if (j - i + 1 > maxLen) {
+                        maxLen = j - i + 1;
+                        start = i;
+                    }
+                }
+            }
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
+
+int main() {
+    Solution sol;
+    string s = "babad";
+    cout << "Longest Palindromic Substring: " << sol.longestPalindrome(s) << endl;
+    return 0;
+}
+
+
+*/
