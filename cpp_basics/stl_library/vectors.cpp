722b757973382f41d93df45e4f744de1cbb6e982
siddharths060 <siddharthsrivishwa@gmail.com>
2025-07-29 18:49:33 +0530

    vectors basics 1

--- cpp_basics/stl_library/vectors.cpp
+++ cpp_basics/stl_library/vectors.cpp
@@ -18,5 +18,37 @@ using namespace std;
 
 int main(){
 
+    // size() returns the size of the vector
+    vector<int> arr = {1,2,3};
+    cout<<arr.size()<<endl;
+
+    //empty() returns false if size of vector is 0 else returns true
+    cout<<arr.empty()<<endl;
+
+    // accessing the elements 
+    // [] operator can be used to access the elements, the indexes start from 0
+    cout<<arr[0]<<endl;
+    // at() can be used to access the element at that perticular position
+    cout<<arr.at(2)<<endl;
+    // front() returns the first element of the vector
+    cout<<arr.front()<<endl;
+
+    // back() returns the last element of the vector
+    cout<<arr.back()<<endl;
+
+    // modifiers
+    // push_back() adds elements to the last or end of the vector
+    arr.push_back(20);
+    cout<<arr.back()<<endl;
+    // pop_back() deletes elements from the last 
+    
+    arr.pop_back();
+    cout<<arr.back()<<endl;
+    // insert(pos,val) returns an iterator to the begining of the new vector
+    arr.insert(arr.begin(), 500);
+    cout<<arr.front()<<endl;
+    // erase() removes either a single element erase(iterator to that position) or a range of elements erase([first,last])
+    arr.erase(arr.begin());
+    cout<<arr.front()<<endl;
     return 0;
 }
